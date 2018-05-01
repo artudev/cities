38ac6e9a25ab4275bcbfa75239a46900997b2251
Artur Kasprzak <artur.kasprzak89@gmail.com>
2018-05-01 19:14:52 +0200

    AssetsHelper and it's tests

---
+++ app/src/main/java/com/assignment/cities/model/assets/AssetHelper.java
@@ -0,0 +1,12 @@
+package com.assignment.cities.model.assets;
+
+import android.content.Context;
+
+/**
+ * Created by Artur Kasprzak on 01.05.2018.
+ */
+public interface AssetHelper {
+
+	String retrieveTextAsset(Context context, String path);
+
+}
